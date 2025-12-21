b50368d616717efaedbcf528a6dc7965ba4d5f3e
Pranesh Meher <praneshmeher@gmail.com>
2025-12-21 22:58:42 +0530

    initial setup

---
+++ src/main/java/com/example/annotation/PrefixResponseAspect.java
@@ -0,0 +1,34 @@
+package com.example.annotation;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Method;
+
+@Aspect
+@Component
+@SuppressWarnings("unused")
+public class PrefixResponseAspect {
+
+    @Around("@annotation(com.example.annotation.PrefixResponse)")
+    public Object around(ProceedingJoinPoint pjp) throws Throwable {
+
+        MethodSignature signature = (MethodSignature) pjp.getSignature();
+        Method method = signature.getMethod();
+        PrefixResponse ann = method.getAnnotation(PrefixResponse.class);
+
+        String prefix = ann.prefix() == null ? "" : ann.prefix();
+        boolean apply = ann.apply();
+
+        Object result = pjp.proceed();
+        if (apply && result instanceof CharSequence cs) {
+            return prefix + cs;
+        }
+
+        return result;
+    }
+}
